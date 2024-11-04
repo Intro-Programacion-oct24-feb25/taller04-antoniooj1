d2eae2c0a240602a0f7b4e7ea88371e764236108
Antonio Ojeda <ajojeda6@utpl.edu.ec>
2024-11-03 21:36:57 -0500

    Cambios bruscos

--- problema03/Problema03/src/problema03/Problema0343.java
+++ problema03/Problema03/src/problema03/Problema0343.java
@@ -17,22 +17,17 @@ public class Problema03 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner scanner = new Scanner(System.in);
-        Double planilla;
-        Double costoMinuto;
-        Double minutosMes;
-        String direccion;
-        String nombreCompleto;
+        
         System.out.println("Ingrese el costo por minuto");
-        costoMinuto = scanner.nextDouble();
+        double costoMinuto = scanner.nextDouble();
         System.out.println("Ingrese el numero de minutos consumidos");
-        minutosMes = scanner.nextDouble();
+        double minutosMes = scanner.nextDouble();
         System.out.println("Ingrese la direccion en la que vive");
         scanner.nextLine();
-        direccion = scanner.nextLine();
+        String direccion = scanner.nextLine();
         System.out.println("Ingrese su nombre completo");
-        nombreCompleto = scanner.nextLine();
-        planilla = (costoMinuto * minutosMes );
-        
+        String nombreCompleto = scanner.nextLine();
+        double planilla = (costoMinuto * minutosMes );
         System.out.printf("Reporte:\nNombres completos: %s\n\tDirecciones: %s\n"
         +"Costo por minuto: %.2f\n\tNumero de minutos consumidos: " 
         + " %.2f\n\t\tValor a cancelar: %.2f\n",
@@ -41,10 +36,3 @@ public class Problema03 {
         costoMinuto,
         minutosMes,
         planilla);
-                
-        
-        
-                
-    }
-    
-}
